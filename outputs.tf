be82b6a7dbc6d5ec4a9f3100f1c7868725e23554
Jared Hatfield <jjhatf02@gmail.com>
2024-08-29 18:26:44 -0400

    output manage_region_service_map

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@
 output "token_region_service_map" {
   value = module.serviceauthcentral_token_gcp.region_service_map
 }
+
+output "manage_region_service_map" {
+  value = module.serviceauthcentral_manage_gcp.region_service_map
+}
